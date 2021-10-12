36bba720863e682fb19a6102c9936733a4b0d121
Txomin Jimenez <txomin.jimenez@gmail.com>
2021-10-12 18:56:24 +0200

    Generate idle sound.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Enginooze
+
+This is an experiment to build a DIY sports exhaust sound generator using an esp32 board. Work in progress.
---
+++ main/enginooze.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <driver/dac.h>
+#include "sounds/idle.h"
+
+static const char *TAG = "Enginooze";
+volatile int sample_idx = 0;
+static const int SAMPLE_INTERVAL_11025 = 90;
+
+void enable_sound(void)
+{
+    esp_err_t err = dac_output_enable(DAC_CHANNEL_1);
+    ESP_ERROR_CHECK(err);
+}
+
+void set_sample(void *arg)
+{
+    dac_output_voltage(DAC_CHANNEL_1, sound_idle[sample_idx++]);
+    if (sample_idx >= sound_idle_len)
+        sample_idx = 0;
+}
+
+void idle(void)
+{
+    const esp_timer_create_args_t timargs = {
+        .callback = &set_sample,
+        .name = "enginesound"};
+    esp_timer_handle_t every;
+    esp_timer_create(&timargs, &every);
+    esp_timer_start_periodic(every, SAMPLE_INTERVAL_11025);
+}
+
+void app_main(void)
+{
+    ESP_LOGI(TAG, "Started!");
+    enable_sound();
+    idle();
+    while (1)
+    {
+        vTaskDelay(100000 / portTICK_PERIOD_MS);
+    };
+    ESP_LOGI(TAG, "Finished!");
+}
