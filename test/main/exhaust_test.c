fda15fb83ee91f89b53043cb1bcc658f53ae7f5e
Txomin Jimenez <txomin.jimenez@gmail.com>
2022-01-07 19:31:10 +0100

    Fix exhaust tests

--- test/main/exhaust_test.c
+++ test/main/exhaust_test.c
@@ -19,10 +19,10 @@ void exhaust_test_after_each(void) {
 void test_does_not_emit_sound_when_engine_stopped()
 {
     exhaust_init(test_sound_device, idle_rpm);
-    int engine_rpm = 1000;
+    int engine_rpm = idle_rpm;
     exhaust_on_update(engine_rpm);
     engine_rpm = 0;
-    
+
     exhaust_on_update(engine_rpm);
 
     TEST_ASSERT_EQUAL(0, test_sound_device.volume);
@@ -31,7 +31,7 @@ void test_does_not_emit_sound_when_engine_stopped()
 void test_emits_sound_when_engine_is_running()
 {
     exhaust_init(test_sound_device, idle_rpm);
-    int engine_rpm = 1000;
+    int engine_rpm = idle_rpm;
 
     exhaust_on_update(engine_rpm);
 
@@ -49,7 +49,7 @@ void test_adjust_sound_pitch_with_rpm_changes()
     TEST_ASSERT_EQUAL(0.01, test_sound_device.pitch);
 
     exhaust_on_update(1440);
-    TEST_ASSERT_EQUAL(0.44, test_sound_device.pitch);    
+    TEST_ASSERT_EQUAL(0.44, test_sound_device.pitch);
 }
 
 int run_exhaust_test()
@@ -59,7 +59,7 @@ int run_exhaust_test()
     UnityBegin("Exhaust");
     exhaust_test_before_all();
     RUN_EXHAUST_TEST(test_does_not_emit_sound_when_engine_stopped);
-    RUN_EXHAUST_TEST(test_does_emits_sound_when_engine_is_running);
-    RUN_EXHAUST_TEST(test_does_adjust_sound_pitch_with_rpm_changes);
+    RUN_EXHAUST_TEST(test_emits_sound_when_engine_is_running);
+    RUN_EXHAUST_TEST(test_adjust_sound_pitch_with_rpm_changes);
     return UNITY_END();
 }
